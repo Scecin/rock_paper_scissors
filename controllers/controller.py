ddcf71cc3e35351f820076b74c4a6395361feacf
Scecin <silvia.cecin@hotmail.com>
2022-04-15 11:42:10 +0100

    played_1 and played_2 form included

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,7 @@
+from flask import render_template, template_rendered
 from app import app
+from models.player import Player
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template("index.html", title="Home")
--- models/players.py
+++ models/players.py
@@ -2,4 +2,7 @@ from models.player import Player
 
 
 player_1 = Player("david", "scissors")
-player_2 = Player("Anna", "rock")                 
+player_2 = Player("Anna", "rock")  
+player_3 = Player("Mary", "paper")  
+
+players = [player_1, player_2, player_3]
